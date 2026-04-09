267e72ddb9429e97aae24262640e2562ac90ea16
isaac gonzalez <isa.gonzalezl@duocuc.cl>
2026-04-08 23:47:29 -0400

    navbar creado y quite estilos predeterminados

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { home } from './pages/Home'
 import Recomendados from './Componets/Recomendados'
+import Navbar from './Componets/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
     <Recomendados/>
       
     </>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const home = () => {
+    return (
+        <div className="home-page">
+            <section id="hero-section">
+                
+            </section>
+        </div>
+    )
+}
