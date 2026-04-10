0642bbb50cdf5069488b156a02fb2eb97e5b0a2b
Almendra Pereira <alm.pereira@duocuc.cl>
2026-04-10 17:36:32 -0400

    creacion de footer, se agrego el mapa y se modifico la parte de los productos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Recomendados from './Componets/Recomendados'
+import Ubicacion from './Componets/Ubicacion'
+import Footer from './Componets/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +14,10 @@ function App() {
     <>
     <Recomendados/>
       
+       <Ubicacion/>
+       <Footer/>
     </>
+   
   )
 }
 
